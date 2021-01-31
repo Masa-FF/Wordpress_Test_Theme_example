42d32e31c0e71bc667e9d3d7d5e625e11d03b1b5
Masa-FF <na7stea2@gmail.com>
2021-01-31 23:58:22 +0900

    Add_DOCTYPE Style.css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test_Wordpress_example</title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime( get_stylesheet_directory() . '/style.css'); ?>" type="text/css" />
+</head>
+
+<header>
+  <h1>My Photo Album</h1>
+</header>
+
+<body>
+
+</body>
+
+<footer>
+</footer>
+
+</html>
