8fa73afa2a4575e9b75bb64bb2168b72cfd2635b
Masa-FF <na7stea2@gmail.com>
2021-02-02 22:22:36 +0900

    Add_Footer_Header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Photographs Album</title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <?php wp_head(); ?>
+  <!-- WordPressで必要なファイルを設定するために必要な記述。headの最下段に記述する -->
+</head>
+
+<body <?php body_class(); ?> >
+  <header>
+    <h1>ポートレートアルバム</h1>
+    <nav>
+      <li><a href"">top</a></li>
+      <li><a href"">about</a></li>
+      <li><a href"">link</a></li>
+    </nav>
+  </header>
