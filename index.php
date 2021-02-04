645f5512117191cd8b9c5e9e2441f7bc38b24557
Masa-FF <na7stea2@gmail.com>
2021-02-05 00:19:53 +0900

    Edit_style.css_Add_index.html_php_while

--- index.php
+++ index.php
@@ -3,15 +3,13 @@
 
 <aside>
   <h3>サイドバー</h3>
+  <?php while(have_posts()) : the_post(); ?>
+    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <p><?php the_content(); ?></p>
+  <?php endwhile; ?>
 </aside>
 
 <main>
-  <?php the_post(); ?>
-  <a href="post.html">
-    <?php the_title(); ?>
-  </a>
-  <p>投稿時刻 : <?php the_time("Y-m-d"); ?></p>
-  <?php the_content(); ?>
 </main>
 
 <?php get_footer(); ?>
