36355579a82f73bb2d6415b723a62f392deb5da0
Masa-FF <na7stea2@gmail.com>
2021-02-07 23:40:25 +0900

    Add_レスポンシブ対応_jquery(スマホ側メニュー)

--- header.php
+++ header.php
@@ -23,4 +23,29 @@
       <li><a href"">About</a></li>
       <li><a href"">Link</a></li>
     </nav>
+
+    <nav class="sp-nav">
+      <ul>
+        <li><a href="#">Photos</a></li>
+        <li><a href="#">Blogs</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="close"><span>Close</span></li>
+      </ul>
+    </nav>
+
+<!-- jQueryの読み込み -->
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+<!-- スマホナビの表示・非表示 -->
+<script>
+$(function() {
+   const hum = $('#hamburger, .close')
+   const nav = $('.sp-nav')
+   hum.on('click', function(){
+      nav.toggleClass('toggle');
+   });
+});
+</script>
+
   </header>
