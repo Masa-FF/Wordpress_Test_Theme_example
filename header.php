d733f2618f4b0513d92cf3915045fac113bd4bc9
Masa-FF <na7stea2@gmail.com>
2021-02-11 15:26:34 +0900

    Add_jpuery_humberbarmenu

--- header.php
+++ header.php
@@ -17,8 +17,19 @@
   <!-- FontAwesome -->
   <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/CSS/slick.css">
   <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/CSS/slick-theme.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all"/>
   <!-- Slicer -->
+  <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="crossorigin="anonymous"></script>
+  <script>
+    $(function() {
+      const hum = $('#hamburger, .close')
+      const nav = $('.sp-nav')
+      hum.on('click', function() {
+        nav.toggleClass('toggle');
+      });
+    });
+  </script>
+  <!-- ハンバーガーメニューアニメーション -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all"/>
   <?php wp_head(); ?>
   <!-- WordPressで必要なファイルを設定するために必要な記述。headの最下段に記述する -->
 </head>
@@ -36,29 +47,14 @@
       <li><a href="<?php echo esc_url(home_url("/profile/")); ?>">PROFILE</a></li>
       <li><a href="<?php echo esc_url(home_url("/link/")); ?>">Link</a></li>
     </nav>
-
     <nav class="sp-nav">
       <ul>
-        <li><a href="#">Gallery</a></li>
-        <li><a href="#">Blogs</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="<?php echo esc_url(home_url("//")); ?>#photos"">Gallery</a></li>
+        <li><a href="<?php echo esc_url(home_url("/blogs/")); ?>">Blogs</a></li>
+        <li><a href="<?php echo esc_url(home_url("/about/")); ?>">About</a></li>
+        <li><a href="<?php echo esc_url(home_url("/profile/")); ?>">Profile</a></li>
+        <li><a href="<?php echo esc_url(home_url("/link/")); ?>">Link</a></li>
         <li class="close"><span>Close</span></li>
       </ul>
     </nav>
-
-    <!-- jQueryの読み込み -->
-    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
-
-    <!-- スマホナビの表示・非表示 -->
-    <script>
-      $(function() {
-        const hum = $('#hamburger, .close')
-        const nav = $('.sp-nav')
-        hum.on('click', function() {
-          nav.toggleClass('toggle');
-        });
-      });
-    </script>
   </header>
