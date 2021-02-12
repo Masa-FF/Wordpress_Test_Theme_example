d800321b75ef97083c6de20d178f1a2e9b53df34
Masa-FF <na7stea2@gmail.com>
2021-02-12 19:59:05 +0900

    Add_single.php_stylesheet

--- 404.php
+++ 404.php
@@ -2,7 +2,5 @@
   <section>
     <h2>404 Not found</h2>
     <p>お探しのページは存在しないか移動しました。</p>
-    <?php get_search_form(); ?>
-    <!-- 検索フォームが表示される -->
   </section>
 <?php get_footer() ?>
--- single.php
+++ single.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
-  <section>
+  <main>
     <?php if( have_posts()): while( have_posts()): the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <p><?php echo get_the_date('Y年n月j日'); ?></p>
+    <div class="single-page-data">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <p>撮影日: <?php echo get_post_meta($post->ID, '撮影日', true); ?></p>
+    </div>
     <div class ="contents_area">
       <?php the_content(); ?>
     </div>
     <?php endwhile; endif; ?>
-  </section>
+  </main>
 <?php get_footer(); ?>
